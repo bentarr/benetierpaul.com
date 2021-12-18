c7a19664dc4bb8330d309650dc4a8083303b9a53
bentarr <benetierpaul@gmail.com>
2021-12-19 00:55:50 +0100

    Header Stucture finished

--- src/components/Header/HeaderStyles.jsx
+++ src/components/Header/HeaderStyles.jsx
@@ -120,11 +120,11 @@ export const SocialIcons = styled.a`
 transition: 0.3s ease;
 color: white;
 border-radius: 50px;
+margin-top: -8px; 
   padding: 8px;
 &:hover {
     background-color: #212d45;
     transform: scale(1.2);
     cursor: pointer;
-    
   }
 `
