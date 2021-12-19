834e00f53cf4eae7e62f1b2e6ade1842de0526fa
bentarr <benetierpaul@gmail.com>
2021-12-19 19:55:35 +0100

    Home structure + animation finished

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,9 +5,18 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Bienvenue sur <br/>
+        Mon Site 
+      </SectionTitle>
+      <SectionText>
+        Le but de mon site est de montrer mes différentes réalisations et ainsi de vous proposer mes services dans vos projets de création sur Internet. 
+      </SectionText>
+      <Button onClick={() => window.location = 'https://google.com'}>En voir plus</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
