f6a5233aff2bfce5ec8e8b5e16d2165d4bdfd658
bentarr <benetierpaul@gmail.com>
2021-12-26 23:47:32 +0100

    fix for prod

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => (
     <Div1>
       <Link href="/">
         <a style={{ display: 'flex', alignItems: 'center', color: 'white'}}>
-          <Span>paulBen</Span>
+          <Span>PaulBen</Span>
         </a>
       </Link>
     </Div1>
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,8 +8,8 @@ import { LeftSection } from './HeroStyles';
 const Hero = (props) => (
   <Section nopadding>
       <SectionTitle main center>
-        paulBen - <br/>
-        intégrateur et développeur web 
+        Paul Bénétier,<br/>
+        au coeur de vos envies digitales
       </SectionTitle>
       <SectionText>
         Le but de mon site est de montrer mes différentes réalisations et ainsi de vous proposer mes services dans vos projets de création sur Internet. 
